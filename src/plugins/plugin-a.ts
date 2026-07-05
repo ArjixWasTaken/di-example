0ee218ee5bb280b6c8ad2210e9ba0cb58fe22d46
ArjixWasTaken <me@arjix.dev>
2026-07-05 03:28:36 +0300

    oopsie

--- src/plugins/plugin-a.ts
+++ src/plugins/plugin-a.ts
@@ -1,3 +1,6 @@
 import { container, tokens } from "../container.js";
 
-container.register(tokens.PluginSettings, { useValue: [{ type: "boolean", name: "plugin-a.bool", default: true }] });
+// prettier-ignore
+container.register(tokens.PluginSettings, { useValue: [
+    { type: "boolean", name: "plugin-a.bool", default: true }
+] });
